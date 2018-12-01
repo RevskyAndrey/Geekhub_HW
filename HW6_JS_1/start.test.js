d9a2455528b25e40150c58058c9acda8230445ee
RevskyAndrey <andrey.g1362@gmail.com>
2018-12-01 17:36:24 +0200

    finish test

--- HW6_JS_1/start.test.js
+++ HW6_JS_1/start.test.js
@@ -42,24 +42,53 @@ test('Numbers', () => {
     expect(Number.isInteger(numb/0.3)).toEqual( false); //+
 });
 
+//-------------------------//
+
+
 
 test('test_arrays', () => {
-    expect(string_arrey.length).toEqual(4);  //  +
-     expect(
-         
-     ).toEqual( ); //+
-    // expect(  ).toEqual( ); //+
-    // expect(  ).toEqual( ); //+
-    // expect(  ).toEqual( ); //+
-    // expect(  ).toEqual( ); //+
-    // expect(  ).toEqual( ); //+
-    // expect(  ).toEqual( ); //+
-    // expect(  ).toEqual( ); //+
+
+   string_arrey.push('first'); // добавляем в масив последний элемент
+     expect(string_arrey.length).toEqual(5); //+
+    /****/
+    string_arrey.pop(); // удаляем из  масива последний элемент
+    expect(string_arrey.length).toEqual(4);
+
+    string_arrey.shift();  // удалем из масива поервый элемент
+    expect(string_arrey[0]).toEqual("test"); // not my
+    string_arrey.unshift('your'); // добавляем в масив первый элемент
+    expect(string_arrey[0]).toEqual("your"); // not my
+
+    expect(string_arrey.join()  ).toEqual('your,test,string,arrey' ); //+
+    expect(string_arrey.join(' ')).toEqual('your test string arrey' ); //+
+});
+
+let x = 5,
+    y=10;
+
+
+test('test_compare_operators', () => {
+    expect(5+5*5 ).toEqual(30);
+    expect( (5+5)*5  ).toEqual(50 ); //+
+    expect( 2+5-2/2  ).toEqual(6 ); //+
+    expect( 2 != 5 ).toEqual(true ); //+
+    expect( x < y  ).toEqual( true); //+
+    expect( x+y /x ).toEqual( 7 ); //+
+    expect( y*x /y*y > x  ).toEqual( true); //+
+    expect( x < y ).toEqual( true ); //+
+    expect( false < true  ).toEqual( true ); //+
+    expect( false < 1 ).toEqual( true ); //+
 
 });
-// expect(  ).toEqual( ); //+
-// expect(  ).toEqual( ); //+
-// expect(  ).toEqual( ); //+
-// expect(  ).toEqual( ); //+
-// expect(  ).toEqual( ); //+
-// expect(  ).toEqual( ); //+
+
+
+test('test_equality_comparison_algorithm', () => {
+    expect(null != 0).toEqual(true);
+    expect(false+true).toEqual(1);
+    expect(y /"2").toEqual(5);
+    expect(1024 +'number' + 1024).toEqual('1024number1024');
+    expect('true' == true).toEqual(false);
+    expect('false' === false ).toEqual(false);
+    expect(x == '5' ).toEqual(true);
+    expect('8bit=' + '1byte').toEqual('8bit=1byte');
+});
