82469013af035cc7a326f450a5492cd5932035e3
Revsky Andrey <andrey.g1362@gmail.com>
2018-12-21 13:25:20 +0200

    add class js_button !! Good idea

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -5,32 +5,35 @@ let btn_prev = document.querySelector('#slider .buttons .arrow-left');
 let btn_next = document.querySelector('#slider .buttons .arrow-right');
 
 let images = document.querySelectorAll('#slider .photos img');
-let item = document.querySelectorAll('#slider .button-list li');
-let i = 3;
+let item = document.querySelectorAll(' .js_button');
+let i = 2;
 console.log (images);
 console.log (item); // ???
+
 btn_prev.onclick = function(){
     images[i].className = '';
-    i = i - 1; /* i-- */
-    console.log(i);
+     i-- ;
+
 
     if(i < 0){
         i = images.length - 1;
     }
 
     images[i].className = 'showed';
+
 }
 
 btn_next.onclick = function(){
     images[i].className = '';
-    i = i + 1; /* i++ */
-    console.log(i);
+     i++ ;
+
 
     if(i >= images.length){
         i = 0;
     }
 
     images[i].className = 'showed';
+
 }
 
 });
