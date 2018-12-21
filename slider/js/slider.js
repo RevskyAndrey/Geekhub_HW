5dc798b37e4b2b5f31fdf487d0ed32ade8637f6d
Revsky Andrey <andrey.g1362@gmail.com>
2018-12-21 12:18:22 +0200

    edit HTML , CSS

---
+++ slider/js/slider.js
@@ -0,0 +1,37 @@
+'use strict';
+
+document.addEventListener(DOMContentLoaded, function() {
+
+let btn_prev = document.querySelector('#slider .buttons .arrow-left');
+let btn_next = document.querySelector('#slider .buttons .arrow-right');
+
+let images = document.querySelectorAll('#slider .photos img');
+let i = 3;
+console.log (images);
+
+btn_prev.onclick = function(){
+    images[i].className = '';
+    i = i - 1; /* i-- */
+    console.log(i);
+
+    if(i < 0){
+        i = images.length - 1;
+    }
+
+    images[i].className = 'showed';
+}
+
+btn_next.onclick = function(){
+    images[i].className = '';
+    i = i + 1; /* i++ */
+    console.log(i);
+
+    if(i >= images.length){
+        i = 0;
+    }
+
+    images[i].className = 'showed';
+}
+
+
+});
