51c793a229c3d0f0062b13d6aaaff0b20c288ca6
RevskyAndrey <andrey.g1362@gmail.com>
2018-12-30 22:49:47 +0200

    edit slider - addEventListener in button; need sleep) need add function for items !!

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -1,43 +1,71 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-let btn_prev = document.querySelector('#slider .buttons .arrow-left');
-let btn_next = document.querySelector('#slider .buttons .arrow-right');
-let images = document.querySelectorAll('#slider .photos img');
-let item = document.querySelectorAll(' .js_button');
-let i = 2;
+    let btn_prev = document.querySelector('#slider .arrow-left');
+    let btn_next = document.querySelector('#slider .arrow-right');
+    let images = document.querySelectorAll('#slider .photos img');
+    let item = document.querySelectorAll('#slider .js_button');
+// let item_content = document.querySelectorAll('#slider .js_content');
+    let i = 0;
+    let items;
 
-
-btn_prev.onclick = function(){
-    images[i].className = '';
-    item[i].classList.remove('active');
-     i-- ;
-
-
-    if(i < 0){
-        i = images.length - 1;
+    for (let i = 0; i < item.length; i++) {
+        items = item.item(i);
+        items.addEventListener("click", function (ent) {
+            this.classList.toggle("active");
+            console.log(i);
+            showSlides(i)
+        });
     }
-
-    images[i].className = 'showed';
-    item[i].classList.add('active');
-};
-
-btn_next.onclick = function(){
-    images[i].className = '';
-    item[i].classList.remove('active');
-
-     i++ ;
+    ;
 
 
-    if(i >= images.length){
-        i = 0;
-    }
+    btn_prev.addEventListener("click", function () {
 
-    images[i].className = 'showed';
-    item[i].classList.add('active');
-}
+        images[i].className = '';
+        this.classList.toggle("active");
+        i--;
+        if (i < 0) {
+            i = images.length - 1;
+        }
+
+        images[i].className = 'showed';
+        this.classList.toggle("active");
+    });
+
+    btn_next.addEventListener("click", function () {
+        images[i].className = '';
+        this.classList.toggle("active");
+        i++;
+        if (i >= images.length) {
+            i = 0;
+        }
+
+        images[i].className = 'showed';
+        this.classList.toggle("active");
+
+    });
+
+    function showSlides(n) {
+
+        // let slides = document.getElementsByClassName("slider-item");
+        // let dots = document.getElementsByClassName("dot");
+
+        if (i >= images.length) {
+            i = 0;
+        }
+        images[i].className = 'showed';
+        this.classList.toggle("active");
+
+        if (i < 0) {
+            i = images.length - 1;
+        }
+        images[i].className = 'showed';
+        this.classList.toggle("active");
+    };
 
 });
+
 
 
 
