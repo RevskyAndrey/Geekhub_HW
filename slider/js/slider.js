e6828a801f245c41d6d8349b226bfc78d8976c5e
RevskyAndrey <andrey.g1362@gmail.com>
2018-12-31 15:01:13 +0200

    add need files

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -5,15 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
     let btn_next = document.querySelector('#slider .arrow-right');
     let images = document.querySelectorAll('#slider .photos img');
     let item = document.querySelectorAll('#slider .js_button');
-// let item_content = document.querySelectorAll('#slider .js_content');
+    let item_content = document.querySelectorAll('#slider .content-item');
     let i = 2;
     let items;
 
+    console.log(item_content);
 
     for (let j = 0; j < item.length; j++) {
         items = item.item(j);
         items.addEventListener("click", function (ent) {
-            i=j;
+            i = j;
             showSlides(i)
         });
     };
@@ -23,36 +24,40 @@ document.addEventListener('DOMContentLoaded', function () {
         i--;
         if (i < 0) {
             i = images.length - 1;
-        } ;
+        };
         showSlides(i)
     });
 
 
+    btn_next.addEventListener("click", function () {
+        i++;
+        if (i >= images.length) {
+            i = 0;
+        };
+        showSlides(i)
 
-btn_next.addEventListener("click", function () {
-    i++;
-    if (i >= images.length) {
-        i = 0;
-    };
-    showSlides(i)
-
-});
-
-function dellclass(targets) {
-        item = targets;
-    for (let j = 0; j < item.length; j++) {
-        items = item.item(j);
-        items.classList.remove('active');
-        items.classList.remove('showed');
-    };
-};
+    });
 
     function showSlides(i) {
         console.log(i);
-        dellclass(images);
-        dellclass(item  );
+
+        for (let j = 0; j < item.length; j++) {
+            items = images.item(j);
+            items.classList.remove('showed');
+        };
+        for (let j = 0; j < item.length; j++) {
+            items = item.item(j);
+            items.classList.remove('item-active');
+        };
+        for (let j = 0; j < item.length; j++) {
+            items = item_content.item(j);
+            items.classList.remove('content-active');
+        };
+
+
         images[i].className = 'showed';
-        item[i].classList.toggle("active");
+        item[i].classList.toggle("item-active");
+        item_content[i].classList.toggle("content-active");
     };
 
 
