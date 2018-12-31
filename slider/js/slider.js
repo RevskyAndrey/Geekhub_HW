b6616de315ac3f2a8d8caf345aab7806a8d6ed62
RevskyAndrey <andrey.g1362@gmail.com>
2018-12-31 18:02:12 +0200

    cleaning js  ( kill 12  string cod  | add new function cleaningClass)

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -6,9 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let images = document.querySelectorAll('#slider .photos img');
     let item = document.querySelectorAll('#slider .js_button');
     let item_content = document.querySelectorAll('#slider .content-item');
-    let i ,items;
-
-    console.log(item_content);
+    let i =2,
+        items;
 
     for (let j = 0; j < item.length; j++) {
         items = item.item(j);
@@ -16,49 +15,40 @@ document.addEventListener('DOMContentLoaded', function () {
             i = j;
             showSlides(i)
         });
-    };
-
+    }
 
     btn_prev.addEventListener("click", function () {
         i--;
         if (i < 0) {
             i = images.length - 1;
-        };
+        }
         showSlides(i)
     });
-
 
     btn_next.addEventListener("click", function () {
         i++;
         if (i >= images.length) {
             i = 0;
-        };
+        }
         showSlides(i)
 
     });
 
     function showSlides(i) {
         console.log(i);
-
-        for (let j = 0; j < item.length; j++) {
-            items = images.item(j);
-            items.classList.remove('showed');
-        };
-        for (let j = 0; j < item.length; j++) {
-            items = item.item(j);
-            items.classList.remove('item-active');
-        };
-        for (let j = 0; j < item.length; j++) {
-            items = item_content.item(j);
-            items.classList.remove('content-active');
-        };
-
-
+        cleaningClass(images,'showed');
+        cleaningClass(item,'item-active');
+        cleaningClass(item_content,'content-active');
         images[i].className = 'showed';
         item[i].classList.toggle("item-active");
         item_content[i].classList.toggle("content-active");
-    };
+    }
 
-
+    function cleaningClass (name,classname){
+        for (let j = 0; j < item.length; j++) {
+            items = name.item(j);
+            items.classList.remove(classname);
+        }
+    }
 });
 
