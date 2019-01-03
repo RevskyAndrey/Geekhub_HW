d08dba8077d8d3269ecb5ad72fc40871527e6835
RevskyAndrey <andrey.g1362@gmail.com>
2019-01-03 14:33:31 +0200

    init project

--- HW9_JS4/main.js
+++ HW9_JS4/main.js
@@ -1,18 +1,25 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-    let x =1 ,
-        y=10;
-    let limitN,pageN;
+    let min = 1,
+        max = 10;
 
-function randomN(min,max) {
-    let rand = min + Math.random()*(max+ 1 -min);
-    rand = Math.floor(rand);
-    console.log ("RANDOM ",rand);
-    return rand;
-};
 
-console.log(randomN(x,y));
-setTimeout(randomN(x,y), 5000)
+
+    function randomN(id) {
+        return function () {
+            let rand = min + Math.random() * (max + 1 - min);
+            rand = Math.floor(rand);
+            document.getElementById(id).textContent = rand;
+            console.log(id, rand);
+        }
+    }
+
+    setTimeout(randomN("limitNumber") , 1000);
+
+    setTimeout(randomN("pageNumber"), 2000);
+
+
+
 
 
 
