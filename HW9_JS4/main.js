98da6793cbe55b4a702625072ff344640c24187c
RevskyAndrey <andrey.g1362@gmail.com>
2019-01-03 20:33:31 +0200

    Реализованны функции  : получение рандомного числа limitNumber  - и вывод его на html страницу. получение рандомного числа pageNumber   - и вывод его на html страницу. фунция delay - которая реализуют задержку выполнения функции на заанное время. функция сборки адресса запроса - MyApi реальзован простой запрос на сервер по адрссу который получается после выполнения фунций getlimitNumber , getpageNumber ,get Api и вывод ответа на запрос на html страницу и в консоль

--- HW9_JS4/main.js
+++ HW9_JS4/main.js
@@ -1,26 +1,58 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-    let min = 1,
-        max = 10;
+    let min = 1;
+    let max = 10;
+    let pageNumber;
+    let limitNumber;
+    let MyAPI;
 
+    function delay(f,t) {
+        setTimeout( f,t) ;
+    };
 
+    function getApi() {
+        let str1 = "http://apistaging.theatre.pp.ua/posts.json?limit=";
+        let str2 = "&page=";
+        MyAPI  = str1 +limitNumber +str2 + pageNumber;
+        document.getElementById("MyAPI").textContent = MyAPI;
+        console.log ("API",MyAPI);
+    };
 
-    function randomN(id) {
-        return function () {
-            let rand = min + Math.random() * (max + 1 - min);
-            rand = Math.floor(rand);
-            document.getElementById(id).textContent = rand;
-            console.log(id, rand);
-        }
-    }
+    function getLimitNumber() {
+        limitNumber = min + Math.random() * (max + 1 - min);
+        limitNumber = Math.floor(limitNumber);
+        document.getElementById("limitNumber").textContent = limitNumber;
+        console.log("limitNumber", limitNumber);
+    };
 
-    setTimeout(randomN("limitNumber") , 1000);
+    function getPageNumber() {
+        pageNumber = min + Math.random() * (max + 1 - min);
+        pageNumber = Math.floor(pageNumber);
+        document.getElementById("pageNumber").textContent = pageNumber;
+        console.log("pageNumber", pageNumber);
+    };
 
-    setTimeout(randomN("pageNumber"), 2000);
+    let myXMLHttp = new XMLHttpRequest();
 
+    myXMLHttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.warn(JSON.parse(this.responseText));
+        };
+    };
 
+    function myXMLHttpPrint() {
+        myXMLHttp.open("GET",MyAPI);
+        document.getElementById("Request1").textContent = myXMLHttp;
+        myXMLHttp.send();
+        console.log(myXMLHttp);
+    };
 
+    function go() {
+        delay(getLimitNumber, 1000);
+        delay(getPageNumber, 2000);
+        delay(getApi,3000)
+        delay(myXMLHttpPrint, 6000);
+    };
 
-
-
+go();
 });
