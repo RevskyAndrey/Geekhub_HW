c2445c18a0b9a3b44d824ae5b88e28b13331010c
RevskyAndrey <andrey.g1362@gmail.com>
2019-01-30 22:23:27 +0200

    доделал математику

--- HW10/js/main.js
+++ HW10/js/main.js
@@ -1,4 +1,3 @@
-'use strict';
 // import {showError} from './module.mjs';
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -24,10 +23,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         //  получить растояние
-        getDistance(point_1,point_2) {
-            return Math.sqrt(Math.pow(point_1.x - point_2.x, 2) + Math.pow(point_1.y - point_2.y, 2));
+        getDistance(x1, y1, x2, y2) {
+            return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
         }
-        getradius(point){
+
+        getradius(point) {
             return Math.sqrt(Math.pow(this.y - point.x, 2) + Math.pow(this.x - point.y, 2));
         }
 
@@ -67,59 +67,75 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     class Polygon extends Shape {
-        constructor(center, [point1 ,point2,point3]) {
+        constructor(center, points) {
             super(center);
-            this.x1 = point1.x;
-            this.y1 = point1.y;
-            this.x2 = point2.x;
-            this.y2 = point2.y;
-            this.x3 = point3.x;
-            this.y3 = point3.y;
-            this.side1 = point.getDistance(center,point1);
-            this.side2 = point.getDistance(point1,point2);
-            this.side3 = point.getDistance(point2,point3);
-            this.side4 = point.getDistance(point3,center);
+
         }
+
         get perimeter() {
-            return (this.side1+this.side2+this.side3+this.side4)
+            points = {point1, point2, point3};
+
+            let
+                x1 = point1.x,
+                y1 = point1.y,
+                x2 = point2.x,
+                y2 = point2.y,
+                x3 = point3.x,
+                y3 = point3.y,
+                side1 = point.getDistance(this.x, this.y, x1, y1),
+                side2 = point.getDistance(x1, y1, x2, y2),
+                side3 = point.getDistance(x2, y2, x3, y3),
+                side4 = point.getDistance(x3, y3, this.x, this.y);
+            return (side1 + side2 + side3 + side4)
         }
 
 
     }
 
-    class Rectangle extends Polygon{
+    class Rectangle extends Polygon {
         constructor(center, width, height) {
             super(center);
+            this._width = width;
+            this._height = height;
+        }
+
+        get perimeter() {
+                return(2 *(this._width + this._height));
+        }
+
+        get area() {
+            return (this._width * this._height);
+        }
     }
 
-
-    let Shapes = {
-        Shape: Shape,
-        Polygon: Polygon,
-        Rectangle: Rectangle,
-        // Square: Square,  //квадрад
-        Circle: Circle,
-        Point: Point
-    };
+    class Square extends Rectangle {
+        constructor(center, width) {
+            super(center, width, width);
+        }
+    }
 
 
     let point = new Point(4, 10);
     let share = new Shape(point);
     let circle = new Circle(share, point.getradius(point));
 
-    let point1 =point.getPointAtOffset((point.x+2), (point.y)+2);
-    let point2 =point.getPointAtOffset((point1.x+4), (point1.y+4));
-    let point3 =point.getPointAtOffset(point2.x, point2.y);
+    let point1 = point.getPointAtOffset(point.x + 2, point.y + 2);
+    let point2 = point.getPointAtOffset((point1.x + 4), (point1.y + 4));
+    let point3 = point.getPointAtOffset(point2.x, point2.y);
+    let points = {point1, point2, point3};
+    let polygon = new Polygon(share, points);
 
-    let polygon = new Polygon(share, [point1,point2,point3]);
-    let rectangle = new Rectangle ()
+    let _width = point.getDistance(point1.x, point1.y, point3.x, point3.y);
+    let _height = point.getDistance(point.x, point.y, point2.x, point2.y);
+    let rectangle = new Rectangle(point, _width,_height);
+    let square = new Square (share,_width,_width);
 
 
     let shapes = {
         Shape: share,
         Polygon: polygon,
         Rectangle: rectangle,
-        // Square: Square,
+       Square: square,
         Circle: circle,
         Point: point
     };
