5350a926aaefa399a4e6880d844e70517eea0e7e
RevskyAndrey <andrey.g1362@gmail.com>
2019-01-31 19:33:36 +0200

    getDistance accept only one argument - Point теперь можно вводить свои координаты с html думаю на координатами для  Многоугольника

---
+++ HW10/js/class.mjs
@@ -0,0 +1,102 @@
+export class Point {
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    getPointAtOffset(x1, y1) {
+        return new Point(this.x + x1, this.y + y1);
+    }
+
+    getDistance(point) {
+        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+    }
+
+    getradius(point) {
+        return Math.sqrt(Math.pow(this.y - point.x + 1, 2) + Math.pow(this.x - point.y + 2, 2));
+    }
+
+}
+
+export class Shape {
+    constructor(center) {
+        this._x = center.x;
+        this._y = center.y;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    get y() {
+        return this._y;
+    }
+}
+
+export class Circle extends Shape {
+    constructor(center, _radius) {
+        super(center);
+        this.radius = _radius;
+    }
+
+    get area() {
+        return 3.14 * (this.radius * this.radius);
+    }
+
+    get perimeter() {
+        return (2 * (3.14 * this.radius))
+    }
+
+}
+
+export class Polygon extends Shape {
+    constructor(center, points) {
+        super(center);
+    }
+
+    get perimeter() {
+        this.points = [point1, point2, point3];
+        let
+            x1 = point1.x,
+            y1 = point1.y,
+            x2 = point2.x,
+            y2 = point2.y,
+            x3 = point3.x,
+            y3 = point3.y,
+            side1 = point.getDistance(this.x, this.y, x1, y1),
+            side2 = point.getDistance(x1, y1, x2, y2),
+            side3 = point.getDistance(x2, y2, x3, y3),
+            side4 = point.getDistance(x3, y3, this.x, this.y);
+        return (side1 + side2 + side3 + side4)
+    }
+}
+
+export class Rectangle extends Polygon {
+    constructor(center, width, height) {
+        super(center);
+        this._width = width;
+        this._height = height;
+    }
+
+    get perimeter() {
+        return (2 * (this._width + this._height));
+    }
+
+    get area() {
+        return (this._width * this._height);
+    }
+}
+
+export class Square extends Rectangle {
+    constructor(center, width) {
+        super(center, width, width);
+    }
+}
--- HW10/js/main.js
+++ HW10/js/main.js
@@ -1,34 +1,60 @@
 'use strict';
-import {Point,Shape,Circle,Polygon,Rectangle,Square} from "./module.mjs";
+import {Point, Shape, Circle, Polygon, Rectangle, Square} from "./class.mjs";
+import {testError, showError} from "./module.mjs";
 
 document.addEventListener('DOMContentLoaded', function () {
 
-        let point = new Point(5, 10);
-        let share = new Shape(point);
-        let circle = new Circle(share, point.getradius(point));
+    let
+        yourX = document.querySelector('.js_x'),
+        yourY = document.querySelector('.js_y'),
+        yourPoints = document.querySelector('.js_points'),
+        btn = document.querySelector('.js_bnt'),
+        btn_close = document.querySelector('.close');
 
-        // for poligon //
-        let point1 = point.getPointAtOffset(point.x + 2, point.y + 2);
-        let point2 = point.getPointAtOffset((point1.x + 4), (point1.y + 4));
-        let point3 = point.getPointAtOffset(point2.x, point2.y);
-        let points = {point1, point2, point3};
-        let polygon = new Polygon(share, points);
+    btn.addEventListener("click", function () {
+        let myX = yourX.value;
+        let myY = yourY.value;
+        let myPoints = yourPoints.value;
+        let tempX = yourX.value;
+        let error = testError(myX, myY, myPoints);
+        yourX.value = "";
+        yourY.value = "";
+        if (error == false) {
 
-        // for rectangle and square
-        let _width = point.getDistance(point1.x, point1.y, point3.x, point3.y);
-        let _height = point.getDistance(point.x, point.y, point2.x, point2.y);
-        let rectangle = new Rectangle(point, _width, _height);
-        let square = new Square(share, _width, _width);
+            let point = new Point(myX, myY);
+            let share = new Shape(point);
+            let circle = new Circle(share, point.getradius(point));
 
-        let shapes = {
-            Shape: share,
-            Polygon: polygon,
-            Rectangle: rectangle,
-            Square: square,
-            Circle: circle,
-            Point: point
-        };
-        console.warn(shapes);
+            // for poligon //
+            let point1 = point.getPointAtOffset(point.x + 2, point.y + 2);
+            let point2 = point.getPointAtOffset((point1.x + 4), (point1.y + 4));
+            let point3 = point.getPointAtOffset(point2.x, point2.y);
+
+            let points = [point1, point2, point3];
+
+            let polygon = new Polygon(share, points);
+            console.warn(points);
+            // // for rectangle and square
+            // let _width = point.getDistance(points[1]);
+            // let _height = point.getDistance(points[2]);
+            // let rectangle = new Rectangle(point, _width, _height);
+            // let square = new Square(share, _width, _width);
+
+            let shapes = {
+                Shape: share,
+                Polygon: polygon,
+                // Rectangle: rectangle,
+                // Square: square,
+                Circle: circle,
+                Point: point
+            };
+            printLog(shapes);
+        }
+    });
+
+    function printLog(item) {
+        console.warn(item);
+    }
 
 });
 
