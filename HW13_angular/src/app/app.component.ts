f8fecf40bb49078ef38c6a3a3728f2db852effca
RevskyAndrey <andrey.g1362@gmail.com>
2019-02-10 22:48:24 +0200

    работа с инпут

--- HW13_angular/src/app/app.component.ts
+++ HW13_angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HW13-app';
+  tasks = [];
+  title = '';
+
+  addTask() {
+    this.tasks.push(this.title);
+    console.log(this.title);
+   }
+
 }
