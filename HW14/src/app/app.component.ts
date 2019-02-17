98222c8af56e17b45ad758b6066cc64184fac7ef
RevskyAndrey <andrey.g1362@gmail.com>
2019-02-17 22:28:01 +0200

    вынес вывод элемента в компонент

--- HW14/src/app/app.component.ts
+++ HW14/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Todo-v2';
+  // tasks: Array<{ id: number, text: string, status: boolean, removed: boolean, archived: boolean; }>;
+  tasks = [];
+  id = 0;
+  text: string;
+  status: boolean;
+  removed: boolean;
+  archived: boolean;
+
+  addTask() {
+    this.tasks.push({
+      id: this.id,
+      text: this.text,
+      status: this.status = false,
+      isRemoved: this.removed = false,
+      isArchived: this.archived = false
+    });
+    this.text = null;
+    this.id++;
+  }
+
 }
--- HW14/src/app/task-item/task-item.component.ts
+++ HW14/src/app/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-task-item',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-item.component.scss']
 })
 export class TaskItemComponent implements OnInit {
+  @Input() task: { id: number, text: string, isCompleted: boolean, isRemoved: boolean, isArchived: boolean; };
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
 
 }
