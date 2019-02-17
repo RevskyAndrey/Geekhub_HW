0d04456466fc06302743fbc38f72abf713ee3342
RevskyAndrey <andrey.g1362@gmail.com>
2019-02-17 10:07:44 +0200

    init comit

---
+++ HW14/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
