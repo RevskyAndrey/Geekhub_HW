9b7bf7ff0bbc30cc29601c74d065ab631f69c271
RevskyAndrey <andrey.g1362@gmail.com>
2019-03-17 18:19:36 +0200

    save point

--- HW16/src/app/app-routing.module.ts
+++ HW16/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {MainComponent} from './main/main.component';
+import {TasksComponent} from './tasks/tasks.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: '/tasks', component: TasksComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
