3512a68a4922ec9a7a3c8947cb68a18f4b7b800c
RevskyAndrey <andrey.g1362@gmail.com>
2019-03-17 15:54:02 +0200

    add router

--- HW16/src/app/app.module.ts
+++ HW16/src/app/app.module.ts
@@ -1,24 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import {TodoService } from './todo.service';
+import {Routes, RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskItemComponent } from './task-item/task-item.component';
+import {TodoService} from './todo.service';
+
+import {AppComponent} from './app.component';
+import {TasksComponent} from './tasks/tasks.component';
+import {TaskItemComponent} from './task-item/task-item.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import { MainComponent } from './main/main.component';
+
+const appRoutes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'tasks', component: TasksComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    NotFoundComponent
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [TodoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
